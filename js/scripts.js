89d745c2c8282153381462da3b414765586bfc76
Anna Becker and Matthew Newell <mnewell10@gmail.com>
2016-01-28 16:06:42 -0800

    Show wrong guesses.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-function Hangman(words, gameWord, blanksArray) {
+function Hangman(words, gameWord, blanksArray, wrong) {
   this.words = ["cat", "harse", "pag"];
   this.gameWord = "";
   this.blanksArray = [];
+  this.wrong = [];
 }
 
 Hangman.prototype.randomWord = function() {
@@ -20,20 +21,34 @@ Hangman.prototype.replaceWithLetters = function(userGuess) {
   for (var i = 0; i < this.gameWord.length; i++) {
       if (this.gameWord[i] === userGuess) {
         this.blanksArray[i] = userGuess;
+        return this.blanksArray.join("");
       }
-  } return this.blanksArray.join("");
+    }
 }
 
+Hangman.prototype.wrongGuess = function(userGuess) {
+  this.gameWord.join("");
+  for (var i = 0; i < this.gameWord.length; i++) {
+    if (this.gameWord.indexOf(userGuess) === -1) {
+      this.wrong.push(userGuess);
+      return this.wrong;
+    }
+  }
+}
 
 $(document).ready(function() {
 
   var myHangman = new Hangman();
-  $("#hangmanWord").hide(myHangman.randomWord());
+  myHangman.randomWord();
   $("#hangmanResult").append(myHangman.replaceWithBlanks());
 
   $("form#hangmanForm").submit(function(event) {
         var letterGuess = $("input#userInput").val();
         $("#hangmanResult").text (myHangman.replaceWithLetters(letterGuess));
+        $("#wrongGuess").text(myHangman.wrongGuess(letterGuess));
     event.preventDefault();
+    $("input#userInput").val("");
+
+
   });
 });
